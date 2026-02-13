ac33d745913a09076b233502262b711464746277
DougWell07 <douglas.wellington07@gmail.com>
2026-02-13 20:51:30 -0300

    Add files via upload

---
+++ Exercício 2.py
@@ -0,0 +1,5 @@
+km = float(input("Qual a quantidade de quilometros percorrida:"))
+dias = int(input("Qantos dias o carro foi alugado:"))
+(total_pagar) = (0.15 * km) + (60 * dias)
+print("O valor total a ser pago é de: R$ %.2f "% total_pagar)
+
