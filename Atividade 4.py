a03a3ffe399b2e6df42140553dc5a7892a402b02
DougWell07 <douglas.wellington07@gmail.com>
2026-02-20 20:41:36 -0300

    Delete Atividade 4.py

--- Atividade 4.py
+++
@@ -1,8 +0,0 @@
-salario = float(input("Digite o seu salário:"))
-print(salario)
-if salario > 1250.00:
-    salario_sup = salario * 1.10
-    print("O seu novo salário é de:", salario_sup)
-if salario < 1250.00:
-    salario_inf = salario * 1.15
-    print("O seu novo salário é de:", salario_inf)
