e8b43d934dc8b62cfcae19b40564b1c884d2d3b8
DougWell07 <douglas.wellington07@gmail.com>
2026-02-20 20:36:50 -0300

    Add files via upload

---
+++ Atividade 3.py
@@ -0,0 +1,17 @@
+dia=int(input("Digite o número que corresponde ao dia:"))
+print(dia)
+if dia == 1:
+    print("Hoje é Domingo")
+if dia == 2:
+    print("Hoje é Segunda-feira")
+if dia == 3:
+    print("Hoje é terça-feira")
+if dia == 4:
+     print("Hoje é quarta-feira")
+if dia == 5: 
+    print("Hoje é quinta-feira")
+if dia == 6: 
+    print("Hoje é sexta-feira")
+if dia == 7:
+    print("Hoje é sábado")
+
---
+++ atividade 2.py
@@ -0,0 +1,8 @@
+numero1=int(input("Digite um número"))
+print(numero1)
+numero2=int(input("Digite outro numero"))
+print(numero2)
+if numero1 > numero2:
+       print("Maior número é:",  numero1)
+else:
+       print("Maior número é:", numero2)
