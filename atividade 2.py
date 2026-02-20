603d6d6c239eeb519ca6285e5298f8fa073c6ff1
DougWell07 <douglas.wellington07@gmail.com>
2026-02-20 20:40:55 -0300

    Delete atividade 2.py

--- atividade 2.py
+++
@@ -1,8 +0,0 @@
-numero1=int(input("Digite um número"))
-print(numero1)
-numero2=int(input("Digite outro numero"))
-print(numero2)
-if numero1 > numero2:
-       print("Maior número é:",  numero1)
-else:
-       print("Maior número é:", numero2)
