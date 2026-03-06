b23404973f7ffe49d8fba37a317688158cf4e12b
DougWell07 <douglas.wellington07@gmail.com>
2026-03-05 21:48:33 -0300

    Add files via upload

---
+++ aula 3/exercício 4.py
@@ -0,0 +1,6 @@
+idade=int(input("Informe a sua idade:"))
+if idade >= 16:
+    print("Você tem a idade suficiente para votar")
+if idade >= 18:
+        print("Você tem a idade suficiente para ter a carteira de habiliatação")
+
